6117c5e185824a2e6474f5df75218a74ddf86231
tenryuag <tenryuabe@gmail.com>
2026-01-06 12:38:18 +0900

    feat: Add and integrate a global error boundary component to catch and display application errors.

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from '@/App.jsx'
 import '@/index.css'
+import GlobalErrorBoundary from '@/components/GlobalErrorBoundary'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
+  <GlobalErrorBoundary>
     <App />
+  </GlobalErrorBoundary>
 ) 
